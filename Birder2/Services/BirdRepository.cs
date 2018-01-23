c6fb2e107239f487f41483e61c06f22cea8fa231
Andrew <andrew.cross11@gmail.com>
2018-01-23 00:23:10 +0000

    Bird repository added

---
+++ Birder2/Services/BirdRepository.cs
@@ -0,0 +1,34 @@
+﻿using Birder2.Data;
+using Birder2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Birder2.Services
+{
+    public class BirdRepository : IBirdRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public BirdRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Bird>> AllBirdsList()
+        {
+            return await _dbContext.Birds.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Bird>> FilteredBirdsList(string searchString)
+        {
+            return await _dbContext.Birds.Where(h => h.EnglishName.ToUpper().Contains(searchString.ToUpper())).ToListAsync();
+        }
+
+        public async Task<Bird> GetBirdDetails(int? id)
+        {
+            return await _dbContext.Birds.SingleOrDefaultAsync(m => m.BirdId == id);
+        }
+    }
+}
---
+++ Birder2/Services/IBirdRepository.cs
@@ -0,0 +1,15 @@
+﻿using Birder2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Birder2.Services
+{
+    public interface IBirdRepository
+    {
+        Task<IEnumerable<Bird>> AllBirdsList();
+        Task<IEnumerable<Bird>> FilteredBirdsList(string searchString);
+        Task<Bird> GetBirdDetails(int? id);
+    }
+}
