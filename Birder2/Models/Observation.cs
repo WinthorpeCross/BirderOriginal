6a67b25811ddb1448416b51058f087065c4fea92
Andrew <andrew.cross11@gmail.com>
2018-02-02 09:39:46 +0000

    Work on Observation create view

--- Birder2/Models/Observation.cs
+++ Birder2/Models/Observation.cs
@@ -6,34 +6,28 @@ namespace Birder2.Models
 {
     public class Observation
     {
-        /* 
-         * This is for an individual bird observation.
-         * Consider functionality for mulitiple observations.
-         * (different view model for create multiple observations)
-         */
-
+        // This is for an individual bird observation.  Consider functionality for mulitiple observations (different view model for create multiple observations)
         [Key]
         public int ObservationId { get; set; }
 
         [Required]
-        [Display(Name = "Date and time")]
+        [Display(Name = "When?")]
         [DisplayFormat(DataFormatString = "{0:dddd, dd MMMM yyyy HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime ObservationDateTime { get; set; }
 
         //public Geography LocationGeoTest { get; set; } ---> Not supported in EF Core 2.0
-        public string Location { get; set; }
+        public string Location { get; set; }  //use instead of separate lat/long if Geography comes available
         public double LocationLatitude { get; set; }
         public double LocationLongitude { get; set; }
 
         [Required]
+        [Display(Name = "How many?")]
         public int Quantity { get; set; }
 
         public string Note { get; set; }
 
         public DateTime CreationDate { get; set; }
-
         public DateTime LastUpdateDate { get; set; }
-        // Photos
 
         public int BirdId { get; set; }
         public string ApplicationUserId { get; set; }
