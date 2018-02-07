3b1853a5fb3c2481808c76a59e9d76c0c0d8d3dc
Andrew <andrew.cross11@gmail.com>
2018-02-07 22:18:28 +0000

    Photo stream added to Bird Details.  ViewModel added

--- Birder2/Controllers/BirdController.cs
+++ Birder2/Controllers/BirdController.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Birder2.Services;
 using Microsoft.AspNetCore.Authorization;
+using Birder2.ViewModels;
+using FlickrNet;
 
 namespace Birder2.Controllers
 {
@@ -36,13 +38,37 @@ namespace Birder2.Controllers
             {
                 return NotFound();
             }
-            var bird = await _birdRepository.GetBirdDetails(id);
 
-            if (bird == null)
+            // ****************************************
+
+            string myFlickrApiKey = "7700051a31f80a964a5d0037ad5ed564";
+            string myFlickrSecret = "59f50feafa488bad";
+            //string query = "Cyanistes caeruleus";
+
+            Flickr flickr = new Flickr(myFlickrApiKey, myFlickrSecret);
+
+            //var options = new PhotoSearchOptions { Text = query, Extras = PhotoSearchExtras.AllUrls, SafeSearch = SafetyLevel.Safe, MediaType = MediaType.Photos };
+
+            // ****************************************
+
+            var model = new BirdDetailViewModel()
+            {
+                Bird = await _birdRepository.GetBirdDetails(id),
+                //BirdPhotos = flickr.PhotosSearch(options) ---> set species in SERVICE
+            };
+
+            //string query = model.Bird.Species;
+            var options = new PhotoSearchOptions { Text = model.Bird.Species, Extras = PhotoSearchExtras.AllUrls, SafeSearch = SafetyLevel.Safe, MediaType = MediaType.Photos };
+            model.BirdPhotos = flickr.PhotosSearch(options);
+
+
+            //var bird = await _birdRepository.GetBirdDetails(id);
+
+            if (model.Bird == null)
             {
                 return NotFound();
             }
-            return View(bird);
+            return View(model);
         }
     }
 }
---
+++ Birder2/ViewModels/BirdDetailViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Birder2.Models;
+using FlickrNet;
+
+namespace Birder2.ViewModels
+{
+    public class BirdDetailViewModel
+    {
+        public Bird Bird { get; set; }
+        public PhotoCollection BirdPhotos { get; set; }
+    }
+}
