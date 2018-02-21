49b21d3785678d7ceabc0b1600dd117fa4901cbe
Andrew <andrew.cross11@gmail.com>
2018-02-21 18:09:23 +0000

    TweetDay domain model added

---
+++ Birder2/Domain/Models/TweetDay.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Birder2.Models
+{
+    public class TweetDay
+    {
+        [Key]
+        public int TweetDayId { get; set; }
+
+        public DateTime DisplayDay { get; set; }
+
+        [Url]
+        public string TweetUrl { get; set; }
+
+        [Url]
+        public string ImageUrl { get; set; }
+
+        [Url]
+        public string InformationUrl { get; set; }
+
+
+        public int BirdId { get; set; }
+
+        public Bird Bird { get; set; }
+    }
+}
