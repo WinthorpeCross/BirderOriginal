9126270a4b812e1c60c9d697af0dcb8490f91f9f
Andrew <andrew.cross11@gmail.com>
2018-02-27 00:20:14 +0000

    Detailed observation notes properties added

--- Birder2/Domain/Models/Observation.cs
+++ Birder2/Domain/Models/Observation.cs
@@ -10,14 +10,7 @@ namespace Birder2.Models
         [Key]
         public int ObservationId { get; set; }
 
-        [Required]
-        [Display(Name = "When?")]
-        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy HH:mm}", ApplyFormatInEditMode = true)]
-        [DataType(DataType.DateTime)]
-        public DateTime ObservationDateTime { get; set; }
-
         //public Geography LocationGeoTest { get; set; } ---> Not supported in EF Core 2.0
-        //public string Location { get; set; }  //use instead of separate lat/long if Geography comes available
         public double LocationLatitude { get; set; }
         public double LocationLongitude { get; set; }
 
@@ -25,10 +18,31 @@ namespace Birder2.Models
         [Display(Name = "How many?")]
         public int Quantity { get; set; }
 
-        public string Note { get; set; }
+        public string NoteGeneral { get; set; }
 
+        public string NoteHabitat { get; set; }
+        // Note plant life, water sources and vegetation conditions, as well as which of the plants the bird is preferring as you observe it.
+
+        public string NoteWeather { get; set; }
+        // Note temperature, visibility, wind, light level and any weather conditions that affect your observations.Rain, mist, snowfall, accumulated snow, drought and other factors can impact observations.
+
+        public string NoteAppearance { get; set; }
+        // Take copious notes on the bird's appearance, including the brilliance of plumage, any peculiar markings and any outstanding or unusual features such as missing feathers, leucistic patches or signs of illness. Also record the bird's gender if possible.
+        // Gender
+
+        public string NoteBehaviour { get; set; }
+        // Take notes on what the bird was doing as you observed it.Note general actions and specific reactions to changing conditions, such as the appearance of a predator or how the bird interacts with other birds.Note large actions such as preening, flight patterns and foraging habits as well as small movements such as tail bobs, head cocks or wing stretches.
+
+        public string NoteVocalisation { get; set; }
+        // If the bird sang or made other sounds during your observation, use mnemonics or descriptions of how it sounded. Also note non-vocal sounds such as wing noises or drumming.
+
+
+        [Required]
+        [Display(Name = "When?")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy HH:mm}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.DateTime)]
+        public DateTime ObservationDateTime { get; set; }
         public DateTime CreationDate { get; set; }
-
         public DateTime LastUpdateDate { get; set; }
 
         [Display(Name = "Observed species")]
@@ -38,7 +52,6 @@ namespace Birder2.Models
         public Bird Bird { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
         public ICollection<ObservationTag> ObservationTags { get; set; }
-
     }
 }
 
