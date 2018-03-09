a75e087ad1212d3c1135e66528f824d696d61133
Andrew <andrew.cross11@gmail.com>
2018-03-09 23:10:01 +0000

    constructor invoked

--- Birder2/ViewComponents/TopObservationsViewComponent.cs
+++ Birder2/ViewComponents/TopObservationsViewComponent.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Birder2.Services;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace Birder2.ViewComponents
 {
     public class TopObservationsViewComponent : ViewComponent
     {
+        private readonly IAnalysisRepository _analysisRepository;
+
+        public TopObservationsViewComponent(IAnalysisRepository analysisRepository)
+        {
+            _analysisRepository = analysisRepository;
+        }
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            //var bir = await _analysisRepository.BirdCount();
+            var bir = await _analysisRepository.BirdCount();
             return View();
         }
     }
