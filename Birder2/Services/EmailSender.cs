e1a431a0ea03d72bae61bf09469c5db74329c169
Andrew Stuart Cross <andrew.cross11@gmail.com>
2018-05-14 13:28:42 +0100

    Email Sender set up and app deployed

--- Birder2/Services/EmailSender.cs
+++ Birder2/Services/EmailSender.cs
@@ -1,27 +1,20 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 using System.Threading.Tasks;
 
 namespace Birder2.Services
 {
-    // This class is used by the application to send email for account confirmation and password reset.
-    // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
+        //public AuthMessageSenderOptions Options { get; } //Development only, set via Secret Manager
         private readonly IConfiguration _configuration = null;
-        //public Task SendEmailAsync(string email, string subject, string message)
-        //{
-        //    return Task.CompletedTask;
-        //}
-        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor, IConfiguration configuration)
+
+        public EmailSender(IConfiguration configuration) //IOptions<AuthMessageSenderOptions> optionsAccessor)
         {
             _configuration = configuration;
-            Options = optionsAccessor.Value;
+            //Options = optionsAccessor.Value;
         }
-
-        public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
 
         public Task SendEmailAsync(string email, string subject, string message)
         {
