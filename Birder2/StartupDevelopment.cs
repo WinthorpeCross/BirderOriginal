6e84da0ffae9bdc6f5c62d8a5900805b438cfc40
Andrew Stuart Cross <andrew.cross11@gmail.com>
2018-05-17 15:55:32 +0100

    Cosmetic changes to Bird Index

---
+++ Birder2/StartupDevelopment.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Birder2.Data;
+using Birder2.Models;
+using Birder2.Services;
+using AutoMapper;
+using Newtonsoft.Json;
+
+namespace Birder2
+{
+    public class StartupDevelopment
+    {
+        public StartupDevelopment(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.Configure<StorageAccountOptions>(Configuration.GetSection("StorageAccount"));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedEmail = true;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAutoMapper();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.AddSingleton<IMachineClockDateTime, MachineClockDateTime>();
+
+            services.AddTransient<IApplicationUserAccessor, ApplicationUserAccessor>();
+
+            services.AddScoped<IBirdRepository, BirdRepository>();
+            services.AddScoped<IObservationRepository, ObservationRepository>();
+            services.AddScoped<ISideBarRepository, SideBarRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            //ToDo: Work out what type of service these should be - Singletons?
+            services.AddTransient<IStream, StreamService>();
+            services.AddTransient<IFlickrService, FlickrService>();
+
+            services.AddMvc().AddJsonOptions
+                (options => {
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                });
+
+            //services.Configure<MvcOptions>(options =>
+            //{
+            //    options.Filters.Add(new RequireHttpsAttribute());
+            //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //}
+
+            //if (env.IsProduction())
+            //{
+            //    var options = new RewriteOptions()
+            //        .AddRedirectToHttps();
+
+            //    app.UseRewriter(options);
+            //}
+
+            //app.UseWelcomePage()
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Observation}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
