ddcbaca84072eb04e1ae4c3191e58aaaf11cab6d
Andrew Stuart Cross <andrew.cross11@gmail.com>
2018-05-17 16:14:27 +0100

    Class/Interface started

---
+++ Birder2/Services/ImageStorageService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Birder2.Services
+{
+    public interface IImageStorageService
+    {
+        Task<string> StoreImage(string filename, byte[] image);
+    }
+
+    public class ImageStorageService : IImageStorageService
+    {
+        public async Task<string> StoreImage(string filename, byte[] image)
+        {
+            return null;
+        }
+    }
+}
