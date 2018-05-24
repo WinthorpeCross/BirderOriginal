6d76642d063bbfef26bb7f4981bfd8031ae5f9e4
Andrew Stuart Cross <andrew.cross11@gmail.com>
2018-05-24 23:30:58 +0100

    work on resize images

--- Birder2/Services/StreamService.cs
+++ Birder2/Services/StreamService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ImageMagick;
+using Microsoft.AspNetCore.Http;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -8,12 +9,27 @@ namespace Birder2.Services
     {
         public async Task<byte[]> GetByteArray(IFormFile file)
         {
-            using (var ms = new MemoryStream())
+            using (var memoryStream = new MemoryStream())
             {
-                await file.CopyToAsync(ms);
-                return ms.ToArray();
+                await file.CopyToAsync(memoryStream);
+                using (MagickImage image = new MagickImage(memoryStream.ToArray()))
+                {
+                    MagickGeometry size = new MagickGeometry(64, 64);
+                    // This will resize the image to a fixed size without maintaining the aspect ratio.
+                    // Normally an image will be resized to fit inside the specified size.
+                    size.IgnoreAspectRatio = true;
+
+                    image.Resize(size);
+
+                    image.Write(memoryStream);
+
+                    // Save the result
+                    //image.Write(SampleFiles.OutputDirectory + "Snakeware.100x100.png");
+                }
+                return memoryStream.ToArray();
             }
         }
+
     }
 }
 
