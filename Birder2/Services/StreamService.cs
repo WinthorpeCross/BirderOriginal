9b51a1a840f8743976128bc343ea76b69b8a74f9
Andrew Stuart Cross <andrew.cross11@gmail.com>
2018-05-25 10:24:31 +0100

    fiddling with images service

--- Birder2/Services/StreamService.cs
+++ Birder2/Services/StreamService.cs
@@ -12,18 +12,18 @@ namespace Birder2.Services
             using (var memoryStream = new MemoryStream())
             {
                 await file.CopyToAsync(memoryStream);
+                //memoryStream.ToArray();
+
                 using (MagickImage image = new MagickImage(memoryStream.ToArray()))
                 {
                     MagickGeometry size = new MagickGeometry(64, 64);
-                    // This will resize the image to a fixed size without maintaining the aspect ratio.
-                    // Normally an image will be resized to fit inside the specified size.
+
                     size.IgnoreAspectRatio = true;
 
                     image.Resize(size);
 
                     image.Write(memoryStream);
 
-                    // Save the result
                     //image.Write(SampleFiles.OutputDirectory + "Snakeware.100x100.png");
                 }
                 return memoryStream.ToArray();
