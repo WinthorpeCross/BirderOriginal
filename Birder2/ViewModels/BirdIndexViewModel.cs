4aa71529a28538abd0ca077c852c8bed59320036
Andrew Stuart Cross <andrew.cross11@gmail.com>
2018-06-20 21:24:21 +0100

    Updated post regress to test old bird form

--- Birder2/ViewModels/BirdIndexViewModel.cs
+++ Birder2/ViewModels/BirdIndexViewModel.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 
 namespace Birder2.ViewModels
 {
+    //public class BirdIndexViewModel
+    //{
+    //    public bool ShowAllBirds { get; set; }
+    //    public bool ShowInTable { get; set; }
+    //    public int SelectedBirdId { get; set; }
+    //    public PagedResult<Bird> BirdsList { get; set; }
+    //    public IEnumerable<Bird> AllBirdsDropDownList { get; set; }
+    //}
     public class BirdIndexViewModel
     {
         public BirdIndexStatusFilter BirdStatusFilter { get; set; }
