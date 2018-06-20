e16909e808802d24130e9b60e870d76c9e263824
Andrew Stuart Cross <andrew.cross11@gmail.com>
2018-06-20 20:42:40 +0100

    Test3

--- Birder2/ViewModels/SortFilterBirdIndexOptions.cs
+++ Birder2/ViewModels/SortFilterBirdIndexOptions.cs
@@ -2,8 +2,12 @@
 
 namespace Birder2.ViewModels
 {
-
-
+    public class SortFilterBirdIndexOptions
+    {
+        public int SelectedBirdId { get; set; }
+        public bool ShowAllBirds { get; set; }
+        public bool ShowInTable { get; set; }
+    }
 
 
 
