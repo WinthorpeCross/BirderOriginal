5a6a5efb006468064d7a65218c6630021d85048d
Andrew Stuart Cross <andrew.cross11@gmail.com>
2018-06-22 22:10:10 +0100

    Migrations added; privacy level

--- Birder2/Data/Migrations/20180622205306_Privacy level.Designer.cs
+++ Birder2/Data/Migrations/20180622205306_Privacy level.Designer.cs
@@ -4,14 +4,16 @@ using Birder2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Birder2.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180622205306_Privacy level")]
+    partial class Privacylevel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Birder2/Data/Migrations/20180622205306_Privacy level.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Birder2.Data.Migrations
+{
+    public partial class Privacylevel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SelectedPrivacyLevel",
+                table: "Observation",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SelectedPrivacyLevel",
+                table: "Observation");
+        }
+    }
+}
