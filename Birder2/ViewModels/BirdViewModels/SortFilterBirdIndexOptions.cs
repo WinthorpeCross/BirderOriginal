c42d27d0631eb31cbfab0bb5e661e199e2ac6b35
Andrew Stuart Cross <andrew.cross11@gmail.com>
2018-07-11 17:22:16 +0100

    Removing obsolete code

--- Birder2/ViewModels/BirdViewModels/SortFilterBirdIndexOptions.cs
+++ Birder2/ViewModels/BirdViewModels/SortFilterBirdIndexOptions.cs
@@ -2,13 +2,6 @@
 
 namespace Birder2.ViewModels
 {
-    //public class SortFilterBirdIndexOptions
-    //{
-    //    public int SelectedBirdId { get; set; }
-    //    public bool ShowAll { get; set; }
-    //    public bool ShowInTable { get; set; }
-    //}
-
     public class SortFilterBirdIndexOptions
     {
         public int SelectedBirdId { get; set; }
